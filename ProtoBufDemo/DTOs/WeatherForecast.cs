641c7046185f4649d01e08ff37311c10f4b185f9
benpbo <prizeben@gmail.com>
2021-06-04 19:40:24 +0300

    Added weather forecast dto

---
+++ ProtoBufDemo/DTOs/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ProtoBufDemo.DTOs
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public WeatherForecastSummary Summary { get; set; }
+    }
+}
---
+++ ProtoBufDemo/DTOs/WeatherForecastSummary.cs
@@ -0,0 +1,16 @@
+namespace ProtoBufDemo.DTOs
+{
+    public enum WeatherForecastSummary
+    {
+        Freezing,
+        Bracing,
+        Chilly,
+        Cool,
+        Mild,
+        Warm,
+        Balmy,
+        Hot,
+        Sweltering,
+        Scorching
+    }
+}
