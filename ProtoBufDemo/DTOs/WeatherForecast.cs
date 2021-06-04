b7c6db9ed184160a59e2202e5f5c6a6b152dfc1d
benpbo <prizeben@gmail.com>
2021-06-04 23:43:47 +0300

    Added ProtoBuf attributes for serialization

--- ProtoBufDemo/DTOs/WeatherForecast.cs
+++ ProtoBufDemo/DTOs/WeatherForecast.cs
@@ -1,15 +1,20 @@
 using System;
+using ProtoBuf;
 
 namespace ProtoBufDemo.DTOs
 {
+    [ProtoContract]
     public class WeatherForecast
     {
+        [ProtoMember(1)]
         public DateTime Date { get; set; }
 
+        [ProtoMember(2)]
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
+        [ProtoMember(3)]
         public WeatherForecastSummary Summary { get; set; }
     }
 }
