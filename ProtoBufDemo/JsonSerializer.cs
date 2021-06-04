091f4d0ca7d727158bc811374d94b69f3e4461d9
benpbo <prizeben@gmail.com>
2021-06-04 23:38:04 +0300

    Now has generic conversion type

--- ProtoBufDemo/JsonSerializer.cs
+++ ProtoBufDemo/JsonSerializer.cs
@@ -1,13 +1,12 @@
 using System.Text.Json;
 using ProtoBufDemo.Abstractions;
-using ProtoBufDemo.DTOs;
 
 namespace ProtoBufDemo
 {
-    public class JsonWeatherForecastToStringSerializer : ISerializer<WeatherForecast, string>
+    public class JsonSerializer<T> : ISerializer<T, string>
     {
-        public WeatherForecast Deserialize(string value) => JsonSerializer.Deserialize<WeatherForecast>(value);
+        public T Deserialize(string value) => JsonSerializer.Deserialize<T>(value);
 
-        public string Serialize(WeatherForecast value) => JsonSerializer.Serialize<WeatherForecast>(value);
+        public string Serialize(T value) => JsonSerializer.Serialize<T>(value);
     }
 }
