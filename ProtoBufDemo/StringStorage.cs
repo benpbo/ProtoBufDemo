1e79f43e557fdd472a2ddd92b05e32560bda776e
benpbo <prizeben@gmail.com>
2021-06-04 17:37:50 +0300

    Implemented string storage

--- ProtoBufDemo/StringStorage.cs
+++ ProtoBufDemo/StringStorage.cs
@@ -1,18 +1,33 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ProtoBufDemo
 {
     public class StringStorage : IStorage<string, string>
     {
-        public Task<string> Retrieve(string key)
+        private readonly IDictionary<string, string> data = new Dictionary<string, string>();
+
+        private readonly int _storeDelay;
+        private readonly int _retrieveDelay;
+
+        public StringStorage(int storeDelay, int retrieveDelay)
         {
-            throw new NotImplementedException();
+           _storeDelay = storeDelay;
+           _retrieveDelay = retrieveDelay;
         }
 
-        public Task Store(string key, string value)
+        public async Task<string> Retrieve(string key)
         {
-            throw new NotImplementedException();
+            await Task.Delay(_retrieveDelay);
+            return data.TryGetValue(key, out string value) ?
+                value : throw new InvalidOperationException("Key not found in storage");
+        }
+
+        public async Task Store(string key, string value)
+        {
+            await Task.Delay(_storeDelay);
+            data[key] = value;
         }
     }
 }
